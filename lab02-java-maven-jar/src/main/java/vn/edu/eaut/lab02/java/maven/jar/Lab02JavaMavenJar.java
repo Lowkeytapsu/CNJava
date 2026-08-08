6445536fa93c5dafc073073f4a6fdcee05c6f81a
Hoang Viet <156465670+Lowkeytapsu@users.noreply.github.com>
2026-08-08 11:11:23 +0700

    Add files via upload

---
+++ lab02-java-maven-jar/src/main/java/vn/edu/eaut/lab02/java/maven/jar/Lab02JavaMavenJar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vn.edu.eaut.lab02.java.maven.jar;
+
+/**
+ *
+ * @author tient
+ */
+public class Lab02JavaMavenJar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
